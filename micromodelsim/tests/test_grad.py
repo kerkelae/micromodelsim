068691d41d6bf41b9ab83030d8b47c404ee91d59
Beatrice Taylor <82382427+Bea-Taylor@users.noreply.github.com>
2022-11-11 13:49:08 +0000

    added tests for Gradients

--- micromodelsim/tests/test_grad.py
+++ micromodelsim/tests/test_grad.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.testing as npt 
 
 import micromodelsim as mmsim
 
@@ -14,3 +15,19 @@ def test__vec2vec_rotmat():
         assert np.all(
             (k / np.linalg.norm(k) - aligned_v / np.linalg.norm(aligned_v)) < 1e-10
         )
+
+
+def test_Gradient():
+    npt.assert_raises(TypeError, mmsim.Gradient, bvals="bvals")
+    npt.assert_raises(TypeError, mmsim.Gradient, bvecs="bvecs")
+
+    bvecs = np.zeros((3,3))
+    bvals = np.concatenate((np.ones(len(bvecs)-1), 2 * np.ones(len(bvecs)-1)))
+    bvecs = np.vstack((bvecs, bvecs))
+    npt.assert_raises(ValueError, mmsim.Gradient, bvals, bvecs)
+
+    bvecs = np.zeros((3,2))
+    npt.assert_raises(TypeError, mmsim.Gradient, bvecs)
+
+    bvecs = np.zeros((2,3,4))
+    npt.assert_raises(TypeError, mmsim.Gradient, bvecs)
